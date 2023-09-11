7a2528a2ea40d041a1eb93c2c1c3b1b6b4cef3a5
vadve <vadve@VK-PC>
2023-09-11 20:49:23 +0300

    Add project files.

---
+++ Ex_4/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+double s = 0;
+double x = 5;
+for (int i = 1; i < 10; i++)
+{
+    s += Math.Cos(x * i) / Math.Pow(x, i - 1);
+}
+Console.WriteLine(s);
---
+++ Ex_9/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+double s = 0;
+for (int i = 1; i <= 6; i++)
+{
+    double f = 1;
+    for (int j = 1; j <= i; j++)
+    {
+        f *= j;
+    }
+    s += Math.Pow(-5, i) / f;
+}
+Console.WriteLine(s);
