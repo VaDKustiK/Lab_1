9c2df1d3480913255cc7027b6c8167627eef3d90
Vadim Velikanov <vad.velikanov@yandex.ru>
2023-09-12 19:25:17 +0300

    added a comment

--- Ex_9/Program.cs
+++ Ex_9/Program.cs
@@ -3,11 +3,13 @@
 double s = 0;
 for (int i = 1; i <= 6; i++)
 {
+    // factorial
     double f = 1;
     for (int j = 1; j <= i; j++)
     {
         f *= j;
     }
+
     s += Math.Pow(-5, i) / f;
 }
 Console.WriteLine(s);
