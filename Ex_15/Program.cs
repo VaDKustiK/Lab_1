4d56460299c3732847bfa0f915f10eb0269004a4
Vadim Velikanov <vad.velikanov@yandex.ru>
2023-09-12 19:21:59 +0300

    added a new exercise

---
+++ Ex_15/Program.cs
@@ -0,0 +1,23 @@
+﻿int chisl_1 = 1;
+int chisl_2 = 2;
+int znam_1 = 1;
+int znam_2 = 1;
+
+int chisl;
+int znam;
+
+double res = 0;
+
+for (int i = 0; i <= 5; i++)
+{
+    chisl = chisl_2 + chisl_1;
+    znam = znam_2 + znam_1;
+
+    chisl_1 = chisl_2;
+    znam_1 = znam_2;
+    chisl_2 = chisl;
+    znam_2 = znam;
+
+    res = (double) chisl / znam;
+}
+Console.WriteLine(res);
--- Ex_9/Program.cs
+++ Ex_9/Program.cs
@@ -3,7 +3,6 @@
 double s = 0;
 for (int i = 1; i <= 6; i++)
 {
-    // Вычисление факториала
     double f = 1;
     for (int j = 1; j <= i; j++)
     {
