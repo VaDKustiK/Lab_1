bbe2efc58d379b7c017ba8d8a72bbd93a08b7c54
Vadim Velikanov <vad.velikanov@yandex.ru>
2023-09-12 20:44:56 +0300

    exercise 18

---
+++ Ex_18/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Text;
+
+int n = 10;
+
+for (int i = 3; i <= 24; i += 3)
+{
+    n *= 2;
+    Console.OutputEncoding = Encoding.UTF8;
+    Console.WriteLine($"Через {i} часов будет {n} клеток");
+}
