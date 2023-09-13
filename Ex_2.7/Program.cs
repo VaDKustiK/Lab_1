2686a7c16330794416eb44643c2351693db2c229
Vadim Velikanov <vad.velikanov@yandex.ru>
2023-09-13 20:20:17 +0300

    exercise 7(and 8) (level 2)

---
+++ Ex_2.7/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Text;
+
+double x1 = 0;
+double s1 = 10;
+
+// 8 (A)
+int n1 = 0;
+
+while (n1 <= 7)
+{
+    n1 += 1;
+    x1 += s1;
+    s1 *= 1.1;
+}
+Console.OutputEncoding = Encoding.UTF8;
+Console.WriteLine($"Ответ на пункт А: {x1}");
+
+//8 (Б)
+double s2 = 10;
+double c2 = 0;
+int n2 = 0;
+
+while (c2 < 100)
+{
+    n2++;
+    c2 += s2;
+    s2 *= 1.1;
+}
+Console.OutputEncoding = Encoding.UTF8;
+Console.WriteLine($"Ответ на пункт Б: {n2}");
+
+// 8 (В)
+double s3 = 10;
+int n3 = 0;
+
+while (s3 <= 20)
+{
+    n3++;
+    s3 *= 1.1;
+}
+Console.OutputEncoding = Encoding.UTF8;
+Console.WriteLine($"Ответ на пункт В: {n3}");
