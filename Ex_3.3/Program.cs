deaf408cedc03e4aed8f9224de3c83466d3a5fc1
Vadim Velikanov <vad.velikanov@yandex.ru>
2023-09-15 00:10:30 +0300

    exercise 3 (level 3)

---
+++ Ex_3.3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text;
+
+double a = 0.1;
+double b = 1;
+double h = 0.1;
+double stop = 0.0001;
+double s = 0;
+double e = Double.Epsilon;
+
+for (double x = a; x <= b; x += h)
+{
+    double last = 1;
+    s = 0;
+    for (int i = 1; Math.Abs(last) >= stop; i++)
+    {
+        // factorial
+        double f = 1;
+        for (int j = 1; j <= i; j++)
+        {
+            f *= j;
+        }
+
+        last = Math.Cos(i * x) / f;
+        s += last;
+    }
+    Console.WriteLine($"X = {x.ToString("0.0")}, S = {1 + s}, y = {Math.Pow(e, Math.Cos(x)) * Math.Cos(Math.Sin(x))}");
+}
