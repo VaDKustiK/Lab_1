7ec9ac4e66ceafdc97ff724c1eaf6c324b8fe900
Vadim Velikanov <vad.velikanov@yandex.ru>
2023-09-14 22:53:29 +0300

    exercise 1 (level 3)

---
+++ Ex_3.1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text;
+
+double a = 0.1;
+double b = 1;
+double h = 0.1;
+double stop = 0.0001;
+double s = 0;
+
+for (double x = a; x <= b; x += h)
+{
+    double last = 1; // 1 > stop
+    s = 0;
+    for (int i = 0; Math.Abs(last) >= stop; i++)
+    {
+        // factorial
+        double f = 1;
+        for (int j = 1; j <= 2 * i; j++)
+        {
+            f *= j;
+        }
+
+        last = Math.Pow(-1, i) * (Math.Pow(x, 2 * i) / f);
+        s += last;
+        //Console.WriteLine($"X = {x}, i = {i}, f = {f}, last = {last}"); проверка
+    }
+    Console.WriteLine($"X = {x.ToString("0.0")}, S = {s}, y = {Math.Cos(x)}");
+}
